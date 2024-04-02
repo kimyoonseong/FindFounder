cc626eda48a5303d41d38ca538eac1815e55e706
김가영 <gyk7@naver.com>
2024-04-02 09:17:03 +0900

    feat : spring security Id 중복체크 securityconfig 작성후 회원가입  아이디 중복확인  테스트 Fixes : Jira AI02-33

---
+++ findFounder/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,124 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+//@RequiredArgsConstructor
+@Configuration
+//@EnableWebSecurity(debug = true)
+public class SecurityConfig  {
+	
+	@Bean // 스프링 빈으로 등록
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+		
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+//	@Bean
+//	public WebSecurityCustomizer webSecurityCustomizer() {
+//	    return (web) -> web.ignoring().requestMatchers("swagger-ui.html");
+//	}
+	
+	 @Bean
+	    public InMemoryUserDetailsManager userDetailsService() {
+	        UserDetails user = User.withDefaultPasswordEncoder()
+	            .username("user1")
+	            .password("password")
+	            .roles("USER")
+	            .build();
+	        return new InMemoryUserDetailsManager(user);
+	    }
+	 
+	
+	@Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http
+	            // CSRF 보호 기능을 비활성화
+	            .csrf().disable();
+	            
+
+	        return http.build();
+	    }
+	
+//
+//	public class CustomSecurityFilterManager extends AbstractHttpConfigurer<CustomSecurityFilterManager, HttpSecurity>{
+//		
+//		@Override
+//		public void configure(HttpSecurity httpSecurity) throws Exception {
+//			AuthenticationManager authenticationManager = httpSecurity.getSharedObject(AuthenticationManager.class);
+//			
+//			httpSecurity.addFilter(new JwtAuthenticationFilter(authenticationManager));
+//			
+//			super.configure(httpSecurity);
+//		}	
+//	}
+//	
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//		
+//		// 1. CSRF 해제(서버에 인증정보 저장하지 않기 때문)
+//		http.csrf((csrfConfig) -> csrfConfig.disable()) ;
+//		
+//		// 2. iframe 거부 설정
+//		http.headers((headerConfig) -> headerConfig.frameOptions(frameOptionsConfig ->
+//		frameOptionsConfig.sameOrigin()));
+//		
+//		// 3. cors 재설정
+//		http.cors((corConfig) -> corConfig.configurationSource(configurationSource()));
+		
+		// 4.jSessionId 사용 거부
+		
+		
+		// 5. form 로긴 해제 (UsernamePasswordAuthenticationFilter 비활성화) (폼 로그인 비활성화)
+		
+		
+		
+		// 6. 로그인 인증창이 뜨지 않게 비활성화(기본 인증 비활성화)
+		
+		
+		// 7. 커스텀 필터 적용 (시큐리티 필터 교환) 
+		
+		
+		
+		// 8. 인증 실패 처리
+		
+		
+		
+		// 9. 권한 실패 처리
+		
+		
+		
+		// 10. 인증, 권한 필터 설정 - 경로에 대한 인증 설정
+		
+		
+		// 11. 로그아웃 관련 설정
+//	}
+	
+	
+	
+	
+}
+
