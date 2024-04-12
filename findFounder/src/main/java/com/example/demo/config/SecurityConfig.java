96a2655b7ffede2c1c79fd68361bf5f3ba6c0cab
김가영 <gyk7@naver.com>
2024-04-12 17:43:44 +0900

    Feat : 회원부분 프론트 회원가입, 로그인, 회원 탈퇴, 아이디 찾기 fixes : Jira AI02-54

--- findFounder/src/main/java/com/example/demo/config/SecurityConfig.java
+++ findFounder/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
              "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
             "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/api/v1/auth/**",  "/login", "/api/user", "/api/user/dispatch"
             , "/api/user/check", "/api/user/pw/**", "/login.html", "/css/**", "/js/**", "/index.html", "/assets/**","/email.html"
+            , "/board.html",  "/tables.html", "/register.html", "/withdrawl.html", "/password.html"
     };
 	@Autowired
 	public SecurityConfig(JwtUtil jwtUtil) {
